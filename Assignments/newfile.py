c2df44b737082ef87d517047b8cfa637f7e04843
Sir-Andie28 <kamauandrew017@gmail.com>
2026-02-08 09:05:24 +0300

    Add files via upload
    My classwork coding

---
+++ Assignments/newfile.py
@@ -0,0 +1,34 @@
+print('Andrew Kamau')
+print('I am a boy')
+print('Monday','Tuesday','Wednesday','Thursday')
+print('I am','a boy')
+print('Monday'+'Tuesday'+'Wednesday'+'Thursday')
+x='car bought'
+print(x)
+print('x')
+x='carry him'
+print(x)
+print(type(x))
+y=11
+y2='11'
+print(type(y))
+print(type(y2))
+y2=int(y2)
+print(y2)
+z=0.76
+print(type(z))
+teacher=True
+print(teacher)
+print(type(teacher))
+print(x,y,y2,z,teacher)
+name=input('what is my name :')
+  
+print('welcome',name)
+dob=input('when were you born :')
+dob=int(dob)
+age=2026-dob
+print(type(age))
+print(age)
+print('wow you are',age,'years old')
+print(f'wow you are {age} years old')
+print(f'welcome {name} you are {age} years old')
